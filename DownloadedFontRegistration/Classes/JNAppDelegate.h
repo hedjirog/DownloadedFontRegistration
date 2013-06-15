834d7d5f4bc4cea38b37e2cce94aff13beffe229
Jiro Nagashima <hedjirog@gmail.com>
2013-06-16 03:15:29 +0900

    Add project files

---
+++ DownloadedFontRegistration/Classes/JNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JNAppDelegate.h
+//  DownloadedFontRegistration
+//
+//  Created by Jiro Nagashima on 6/16/13.
+//  Copyright (c) 2013 Jiro Nagashima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DownloadedFontRegistration/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DownloadedFontRegistration
+//
+//  Created by Jiro Nagashima on 6/16/13.
+//  Copyright (c) 2013 Jiro Nagashima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JNAppDelegate class]));
+    }
+}
